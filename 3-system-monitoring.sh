eac023d055893d9b0ec4b44ce7640ade6c0a6d00
cloud-devops-practitioner <srewithdevops@outlook.com>
2026-03-05 16:41:22 +0530

    Create 3-system-monitoring.sh

---
+++ 3-system-monitoring.sh
@@ -0,0 +1,17 @@
+ps  #Snapshot of Current Processes
+ps -ef #snapshot of all process
+top #Real Time System Monitoring
+df -h #Disk free space - h shows in human readable format , k, m, g - kilo, mega and giga bytes ; -t 
+du -h #File Space Usage
+free -g #Display Free and Used Memory
+kill #Terminate Processes
+#-9: Forcefully terminate a process.
+#-l: List all signal names.
+#-s [signal]: Specify a signal to send.
+#-p: Print the process ID.
+uptime #System Runtime
+#Current Time: The time at which the command was run.
+#Uptime Duration: How long the system has been running since the last reboot.
+#Number of Users: The number of users currently logged into the system.
+#Load Averages: The system load averages for the past 1, 5, and 15 minutes.
+
